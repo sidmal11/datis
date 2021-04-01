abd957bd58dc06f4e06e5c226cf20cec510c9d78
sidmal11 <39958179+sidmal11@users.noreply.github.com>
2021-04-01 16:50:52 -0400

    Added editing feature,segregeated routes,added spinner

--- datis-ui/src/app/app.module.ts
+++ datis-ui/src/app/app.module.ts
@@ -9,7 +9,7 @@ import {MatToolbarModule } from '@angular/material/toolbar';
 import {MatExpansionModule } from '@angular/material/expansion';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
+import {MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
@@ -32,6 +32,7 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     MatButtonModule,
     MatToolbarModule,
     MatExpansionModule,
+    MatProgressSpinnerModule,
     HttpClientModule
   ],
   providers: [],
