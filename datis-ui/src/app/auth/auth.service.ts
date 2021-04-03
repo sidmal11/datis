4ea02f539b026bf6949c862ac9f5c2771375f654
sidmal11 <39958179+sidmal11@users.noreply.github.com>
2021-04-03 19:14:31 -0400

    error handling added catch blocks

--- datis-ui/src/app/auth/auth.service.ts
+++ datis-ui/src/app/auth/auth.service.ts
@@ -30,10 +30,11 @@ export class AuthService {
   createUser(email: string, password: string) {
     const authData: AuthData = {email: email, password: password};
     this.http.post("http://localhost:3000/api/user/signup", authData)
-      .subscribe(response => {
-        console.log(response);
+      .subscribe(() => {
+        this.router.navigate(["/login"]);
+      }, error => {
+        this.authStatusListener.next(false);
       });
-      this.router.navigate(["/login"]);
   }
 
   autoAuthUser() {
@@ -78,7 +79,9 @@ export class AuthService {
           this.saveAuthData(token, expirationDate, this.userId);
           this.router.navigate(["/"]);
         }
-      });
+      }, error => {
+        this.authStatusListener.next(false);
+      })  ;
   }
 
   logout() {
