e6e49cba6f5e1963ca7920ec2d409b69b14fcfda
sidmal11 <39958179+sidmal11@users.noreply.github.com>
2021-04-03 13:34:20 -0400

    Added guard and interceptors which will guard the routes and responses by adding custom bearer jwt token

--- datis-ui/src/app/app.component.ts
+++ datis-ui/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 import {Post} from './posts/post.model'
 
 
@@ -7,10 +8,10 @@ import {Post} from './posts/post.model'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  storedPosts = [];
+export class AppComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  onPostAdded(post) {
-    this.storedPosts.push(post);
+  ngOnInit() {
+    this.authService.autoAuthUser();
   }
 }
