c63d3a0d1b2856b524632062efdd5af6969a7b75
sidmal11 <39958179+sidmal11@users.noreply.github.com>
2021-04-05 08:50:33 -0400

    added controllers to separate logic

---
+++ datis-ui/backend/controllers/posts.js
@@ -0,0 +1,104 @@
+const Post = require("../models/post");
+
+exports.createPost = (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+    creator: req.userData.userId,
+  });
+
+  console.log(req.body.title);
+  post
+    .save()
+    .then((createdPost) => {
+      res.status(201).json({
+        message: "Post added successfully",
+        postId: createdPost._id,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Employee creation failed" });
+    });
+};
+
+exports.updatePost = (req, res, next) => {
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content,
+    creator: req.userData.userId,
+  });
+  Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post)
+    .then((result) => {
+      if (result.nModified > 0) {
+        res.status(200).json({ message: "Update successful!" });
+      } else {
+        res.status(401).json({ message: "Not authorized!" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Couldnt update employee",
+      });
+    });
+};
+
+exports.getPosts = (req, res, next) => {
+  const pageSize = +req.query.pagesize;
+  const currentPage = +req.query.page;
+  const postQuery = Post.find();
+  let fetchedPosts;
+  if (pageSize && currentPage) {
+    postQuery.skip(pageSize * (currentPage - 1)).limit(pageSize);
+  }
+  postQuery
+    .then((documents) => {
+      fetchedPosts = documents;
+      return Post.count();
+    })
+    .then((count) => {
+      res.status(200).json({
+        message: "Posts fetched successfully!",
+        posts: fetchedPosts,
+        maxPosts: count,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Fetching employee failed",
+      });
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  Post.findById(req.params.id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: "Post not found!" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Fetching employee failed",
+      });
+    });
+};
+
+exports.deletePost = (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId })
+    .then((result) => {
+      console.log(result);
+      if (result.n > 0) {
+        res.status(200).json({ message: "Deletion successful!" });
+      } else {
+        res.status(401).json({ message: "Not authorized!" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Deleting employee failed",
+      });
+    });
+};
--- datis-ui/backend/routes/posts.js
+++ datis-ui/backend/routes/posts.js
@@ -1,111 +1,19 @@
 const express = require("express");
+const PostController = require("../controllers/posts");
 
 const Post = require("../models/post");
 const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
 
-router.post("", checkAuth, (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-    creator: req.userData.userId,
-  });
+router.post("", checkAuth, PostController.createPost);
 
-  console.log(req.body.title);
-  post
-    .save()
-    .then((createdPost) => {
-      res.status(201).json({
-        message: "Post added successfully",
-        postId: createdPost._id,
-      });
-    })
-    .catch((err) => {
-      res.status(500).json({ message: "Employee creation failed" });
-    });
-});
+router.put("/:id", checkAuth, PostController.updatePost);
 
-router.put("/:id", checkAuth, (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content,
-    creator: req.userData.userId,
-  });
-  Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post)
-    .then((result) => {
-      if (result.nModified > 0) {
-        res.status(200).json({ message: "Update successful!" });
-      } else {
-        res.status(401).json({ message: "Not authorized!" });
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({
-        message: "Couldnt update employee",
-      });
-    });
-});
+router.get("", PostController.getPosts);
 
-router.get("", (req, res, next) => {
-  const pageSize = +req.query.pagesize;
-  const currentPage = +req.query.page;
-  const postQuery = Post.find();
-  let fetchedPosts;
-  if (pageSize && currentPage) {
-    postQuery.skip(pageSize * (currentPage - 1)).limit(pageSize);
-  }
-  postQuery
-    .then((documents) => {
-      fetchedPosts = documents;
-      return Post.count();
-    })
-    .then((count) => {
-      res.status(200).json({
-        message: "Posts fetched successfully!",
-        posts: fetchedPosts,
-        maxPosts: count,
-      });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        message: "Fetching employee failed",
-      });
-    });
-});
+router.get("/:id", PostController.getPost);
 
-router.get("/:id", (req, res, next) => {
-  Post.findById(req.params.id)
-    .then((post) => {
-      if (post) {
-        res.status(200).json(post);
-      } else {
-        res.status(404).json({ message: "Post not found!" });
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({
-        message: "Fetching employee failed",
-      });
-    });
-});
-
-router.delete("/:id", checkAuth, (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId })
-    .then((result) => {
-      console.log(result);
-      if (result.n > 0) {
-        res.status(200).json({ message: "Deletion successful!" });
-      } else {
-        res.status(401).json({ message: "Not authorized!" });
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({
-        message: "Deleting employee failed",
-      });
-    });
-});
+router.delete("/:id", checkAuth, PostController.deletePost);
 
 module.exports = router;
