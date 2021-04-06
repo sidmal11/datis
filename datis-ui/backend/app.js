7178ebe7bb9a76e442b6cceae13425cddf737f43
sidmal11 <39958179+sidmal11@users.noreply.github.com>
2021-04-06 19:38:15 -0400

    Reverting to 036d92a15e102db3a175bc506e6cb333db3ecea8

--- datis-ui/backend/app.js
+++ datis-ui/backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
+const path = require("path");
 const postsRoutes = require("./routes/posts");
 const userRoutes = require("./routes/user");
 
